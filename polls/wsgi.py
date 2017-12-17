e7de47fb884603cfb9f5f3b6734cec0f15224465
kuldeepiitkgp <34603844+kuldeepiitkgp@users.noreply.github.com>
2017-12-17 19:33:20 +0530

    change in wsgi

--- polls/wsgi.py
+++ polls/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environment.setdefault("DJANGO_SETTING_MODULE","polls.settings")
+#os.environment.setdefault("DJANGO_SETTING_MODULE","polls.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)

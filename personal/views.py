f10c77a1529e85da0cf962d0d9164362f3d9ef61
kuldeepiitkgp <34603844+kuldeepiitkgp@users.noreply.github.com>
2017-12-19 09:21:43 +0530

    Change in contacts

--- personal/views.py
+++ personal/views.py
@@ -7,8 +7,8 @@ def index (request):
     return render(request, 'personal/home.html')
 	
 def contact (request):
-	return  render(request, 'personal/basic.html', {'content':['connect me on my email','kuldeep.iitkgp@gmail.com']})
+	return  render(request, 'personal/basic.html', {'content':['Connect me on my email','kuldeep.iitkgp@gmail.com']})
 	#return (rend, 'personla/basic.html')
 def eblog (request):
-	return  render(request, 'admin/', {'content':['connect me on my email','kuldeep.iitkgp@gmail.com']})
+	return  render(request, 'admin/', {'content':['Connect me on my email','kuldeep.iitkgp@gmail.com']})
 	#return (rend, 'personla/basic.html')
